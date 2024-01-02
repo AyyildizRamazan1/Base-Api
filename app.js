304a30c9942726fd6bde5bb521f806c6f1591124
AyyildizRamazan1 <ramaz01329@gmail.com>
2024-01-02 21:26:08 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app =express()
+require("dotenv").config()
+require("./src/db/dbConnection")
+const port = process.env.PORT || 5001
+const router =require("./src/routers")
+
+//Middlewares
+app.use(express.json())
+app.use(express.json({limit: "50mb"}))
+app.use(express.urlencoded({limit:"50mb", extended: true, parameterLimit: 50000}))
+
+app.use("/api", router)
+
+app.get("/" , (req , res) => {
+    res.json({
+        message:"Ana Sayfa"
+    })
+})
+
+app.listen(port, () =>{
+    console.log(`Server ${port} portundan çalışıyor ...`);
+})
