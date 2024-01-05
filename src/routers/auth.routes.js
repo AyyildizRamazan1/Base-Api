e1c6a8e4cc83c9385df9fffd53a76e7c5d010b55
AyyildizRamazan1 <ramaz01329@gmail.com>
2024-01-05 17:59:04 +0300

    authentication created

--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -1,7 +1,9 @@
 const router = require("express").Router()
 const {login,register} = require("../controllers/auth.contoller")
-router.post("/login", login)
+const authValidation = require("../middlewares/validations/auth.validation");
 
-router.post("/register",register)
+router.post("/login", login);
+
+router.post("/register", authValidation.register, register);
 
 module.exports = router 
