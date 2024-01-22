e18388e11535e803ed2ec2983c83987aefaf2d05
AyyildizRamazan1 <ramaz01329@gmail.com>
2024-01-22 12:05:43 +0300

    image upload-Multer

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const errorHandlerMiddleware = require("./src/middlewares/errorHandle");
 const cors = require("cors");
 const corsOptions = require("./src/helpers/corsOptions");
 const mongoSanitize = require("express-mongo-sanitize");
+const path = require("path");
 
 //Middlewares
 app.use(express.json());
@@ -16,6 +17,8 @@ app.use(express.json({ limit: "50mb" }));
 app.use(
   express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
 );
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/uploads", express.static(__dirname));
 app.use(cors());
 app.use(
   mongoSanitize({
